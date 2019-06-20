4c65fecac5545b0698c267b0d46b71cfdaae8557
dmitriy28 <chabandima2002@gmail.com>
2019-06-20 21:33:51 +0300

    UPDATE SBORKA EXE

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 
 int main(int argc, char *argv[])
 {
-
+    QApplication::addLibraryPath("bin/plugins");
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
 
     return a.exec();
 }
