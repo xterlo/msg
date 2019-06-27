ed4ae452c7b0848dc9e019d2f21f85649a9ce1cb
dmitriy28 <chabandima2002@gmail.com>
2019-06-27 13:30:20 +0300

    beta and realese update

--- main.cpp
+++ main.cpp
@@ -22,80 +22,83 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
     if(!db.open()) {
         QMessageBox::warning(0,"Ошибка!","Не удалось подключиться к серверу.\nКод ошибки: 0001");
+        exit(0);
     }
-    else {
-        QSettings settings("HKEY_CURRENT_USER\\Software\\IBM_PROFILES\\",QSettings::NativeFormat);
-        foreach (QString key, settings.allKeys()) {
-           QSqlQuery query;
-            if (settings.value(key) != "") {
-                beta_user = 1;
-                QSqlQuery query;
-                query.exec("SELECT * FROM beta_uses WHERE user='"+key+"'");
-                QSqlRecord rec = query.record();
-                query.next();
-                QString active_beta  = query.value(rec.indexOf("active_beta")).toString();
-                if (active_beta == "0") {
-                    beta_user = 0;
-                } else {
-                query.exec("SELECT * FROM beta_version WHERE active='1' ORDER BY id DESC ");
-                QSqlRecord rec = query.record();
-                query.next();
-                QString version_  = query.value(rec.indexOf("version")).toString();
-                if (query.last() == true) {
-                if (version != version_) {
-                QMessageBox msgBox;
-                 msgBox.setText("Вышла бета версия программы.\nЖелаете обновиться?");
-                 msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
-                 msgBox.setDefaultButton(QMessageBox::Yes);
-                 int ret = msgBox.exec();
-                 switch (ret) {
-                    case QMessageBox::Yes:
-                        QMessageBox::information(0,"OK","OK");
-                        system("updater.exe");
-                        w.close();
-                        break;
-                    case QMessageBox::No:
-                        break;
-                 }
-            }
-            }
+    QSettings versionn("HKEY_CURRENT_USER\\Software\\IBM_APP\\",QSettings::NativeFormat);
+    versionn.setValue("version", version);
+
+
+    QSettings settings("HKEY_CURRENT_USER\\Software\\IBM_PROFILES\\",QSettings::NativeFormat);
+    foreach (QString key, settings.allKeys()) {
+        if (settings.value(key) == "1" ) {
+            beta_user = 1;
+            QString login = key;
+            QSqlQuery query;
+            query.exec("SELECT * FROM beta_uses WHERE user='"+login+"'");
+            if (query.last() == false) {
+                settings.setValue(login,"0");
+                beta_user = 0;
+            } else {
+               query.exec("SELECT * FROM beta_uses WHERE active_beta='1' AND user='"+login+"'");
+               if (query.last() == true) {
+                   QSettings settings("HKEY_CURRENT_USER\\Software\\IBM_APP\\",QSettings::NativeFormat);
+                   QString version = settings.value("version").toString();
+                   query.exec("SELECT * FROM beta_version WHERE active='1' ORDER BY id DESC");
+                   QSqlRecord rec = query.record();
+                   query.next();
+                   QString version_  = query.value(rec.indexOf("version")).toString();
+                   if (query.last() == true) {
+                   if (version != version_) {
+                       QMessageBox msgBox;
+                       msgBox.setText("Вышла бета версия программы.\nЖелаете обновиться?");
+                       msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+                       msgBox.setDefaultButton(QMessageBox::Yes);
+                       int ret = msgBox.exec();
+                       switch (ret) {
+                          case QMessageBox::Yes:
+                               system("updater.exe");
+                               exit(0);
+                               break;
+                           case QMessageBox::No:
+                               break;
+                        }
+                   }
                }
+            }
         }
-        }
-        if (beta_user != 1) {
-        QSqlQuery query;
-        query.exec("SELECT * FROM version WHERE version='"+version+"' AND active='0'");
-        if (query.last() == true) {
-           QMessageBox::warning(0,"Ошибка!","Необходимо обновить приложение!");
-           w.close();
-        } else {
+      }
+}
+if (beta_user != 1) {
+    QSqlQuery query;
+    query.exec("SELECT * FROM version WHERE version='"+version+"' AND active='0'");
+    if (query.last() == true) {
+        QMessageBox::warning(0,"Ошибка!","Необходимо обновить приложение!");
+        system("updater.exe");
+        exit(0);
+    } else {
         query.exec("SELECT * FROM version WHERE active='1' ORDER BY id DESC ");
         QSqlRecord rec = query.record();
         query.next();
         QString version_  = query.value(rec.indexOf("version")).toString();
         if (version != version_) {
             QMessageBox msgBox;
-             msgBox.setText("Вышла новая версия программы.\nЖелаете обновиться?");
-             msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
-             msgBox.setDefaultButton(QMessageBox::Yes);
-             int ret = msgBox.exec();
-             switch (ret) {
-                case QMessageBox::Yes:
-                    QMessageBox::information(0,"OK","OK");
+            msgBox.setText("Вышла новая версия программы.\nЖелаете обновиться?");
+            msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+            msgBox.setDefaultButton(QMessageBox::Yes);
+            int ret = msgBox.exec();
+            switch (ret) {
+               case QMessageBox::Yes:
                     system("updater.exe");
-                    w.close();
+                    exit(0);
                     break;
                 case QMessageBox::No:
                     break;
              }
         }
-        }
-    }
-    QSettings setting("HKEY_CURRENT_USER\\Software\\IBM_APP\\",QSettings::NativeFormat);
-    setting.setValue("version", version);
-
-    return a.exec();
-}
+     }
+  }
+return a.exec();
 }
