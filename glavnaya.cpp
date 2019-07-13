8184f4ca7e64062cf2c668fc9d4eac06de3ad2e7
xterlo <xterlo@yandex.ru>
2019-07-14 01:45:04 +0300

    fullscreen

--- glavnaya.cpp
+++ glavnaya.cpp
@@ -1,11 +1,19 @@
 #include "glavnaya.h"
 #include "ui_glavnaya.h"
+#include <QTimer>
+#include <QDesktopWidget>
+#include <QDebug>
+#include <windows.h>
 
 Glavnaya::Glavnaya(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Glavnaya)
 {
     ui->setupUi(this);
+    QDesktopWidget * screen = QApplication::desktop();
+    screen->availableGeometry();
+    this->setWindowFlags(Qt::Window | Qt::FramelessWindowHint | Qt::CustomizeWindowHint );
+
 
 
 }
@@ -19,5 +27,77 @@ void Glavnaya::on_pushButton_clicked()
 {
     emit firstWindow();
     this->close();
+}
+
+void Glavnaya::mousePressEvent(QMouseEvent *event) {
+    QDesktopWidget * screen = QApplication::desktop();
+    screen->availableGeometry();
+    m_nMouseClick_X_Coordinate = event->x();
+    m_nMouseClick_Y_Coordinate = event->y();
+    if(event->button() == Qt::LeftButton && m_nMouseClick_Y_Coordinate<20)
+        {
+           {
+           if(checkfull == false)
+               {
+                 checkmouse = true;
+               }
+
+           }
+
+        }
+    else checkmouse = false;
+
+}
+void Glavnaya::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+        checkmouse = false;
+    }
+    return QWidget::mouseReleaseEvent(event);
+}
+void Glavnaya::mouseMoveEvent(QMouseEvent *event)
+{
+    QDesktopWidget * screen = QApplication::desktop();
+    screen->availableGeometry();
+    if (checkmouse == true){
+    move(event->globalX()-m_nMouseClick_X_Coordinate,event->globalY()-m_nMouseClick_Y_Coordinate);}
+
+}
+
+void Glavnaya::on_exitbutton_clicked()
+{
+
+    exit(0);
+}
+
+void Glavnaya::mouseDoubleClickEvent(QMouseEvent *event)
+{
+    Glavnaya::on_fullscreen_clicked();
+}
+
+
+
+void Glavnaya::on_fullscreen_clicked()
+{
+    QDesktopWidget * screen = QApplication::desktop();
+    checkmouse = false;
+    screen->availableGeometry();
+    if(m_nMouseClick_Y_Coordinate<20)
+        {
+        if (Glavnaya::size().width()==screen->availableGeometry().width() && Glavnaya::size().height()==screen->availableGeometry().height() )
+        {
+            Glavnaya::setGeometry(posx,posy,sizew,sizey);
+            checkfull=false;
+        }
+        else
+        {
+            sizew=Glavnaya::size().width();
+            sizey=Glavnaya::size().height();
+            posx=Glavnaya::pos().x();
+            posy=Glavnaya::pos().y();
+            Glavnaya::setGeometry(0,0,screen->availableGeometry().width(),screen->availableGeometry().height());
+            checkfull=true;
+        }
+        }
 }
 
--- glavnaya.h
+++ glavnaya.h
@@ -1,6 +1,11 @@
 #ifndef GLAVNAYA_H
 #define GLAVNAYA_H
+
+#include "ui_glavnaya.h"
 #include <QWidget>
+#include <QMouseEvent>
+#include <QKeyEvent>
+#include <string>
 
 namespace Ui {
 class Glavnaya;
@@ -10,17 +15,39 @@ class Glavnaya : public QWidget
 {
     Q_OBJECT
 
+
 public:
     explicit Glavnaya(QWidget *parent = nullptr);
     ~Glavnaya();
+
+
 signals:
     void firstWindow();
 
+
 private slots:
     void on_pushButton_clicked();
+    void on_exitbutton_clicked();
+    void on_fullscreen_clicked();
 
 private:
+
     Ui::Glavnaya *ui;
+    void mouseDoubleClickEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    int m_nMouseClick_X_Coordinate;
+    int m_nMouseClick_Y_Coordinate;
+    bool checkmouse;
+    bool checkfull;
+
+    int sizew=Glavnaya::size().width();
+    int sizey=Glavnaya::size().height();
+    int posx=Glavnaya::pos().x();
+    int posy=Glavnaya::pos().y();
+
+
 };
 
 #endif // GLAVNAYA_H
